91dd60375ec9c4e4cdcc4748800d7135fb1a3c7a
liemgumball <liem1762001@gmail.com>
2024-05-08 23:39:39 +0700

    feat: css verify email content

--- src/util/mailer.ts
+++ src/util/mailer.ts
@@ -2,7 +2,7 @@ import EnvVars from '@src/constants/EnvVars'
 import logger from 'jet-logger'
 import nodeMailer from 'nodemailer'
 
-export async function sendEmail(email: string, subject: string, text: string) {
+export async function sendEmail(email: string, subject: string, html: string) {
   try {
     const transporter = nodeMailer.createTransport(EnvVars.MailTransporter)
 
@@ -10,10 +10,67 @@ export async function sendEmail(email: string, subject: string, text: string) {
       from: EnvVars.MailTransporter.auth?.user,
       to: email,
       subject: subject,
-      text: text,
+      html: html,
     })
   } catch (error) {
     logger.err(error)
     throw new Error('Failed to send verify email')
   }
+}
+
+export function getMailContent(link: string) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Verify Your Account</title>
+      <style>
+          /* Add your custom styles here */
+          body {
+              font-family: Arial, sans-serif;
+              line-height: 1.6;
+              margin: 0;
+              padding: 0;
+              background: #FFFFFF;
+              color: hsl(240, 10%, 3.9%);
+          }
+          .container {
+              max-width: 600px;
+              margin: 20px auto;
+              padding: 20px;
+              border: 1px solid #F1F1F1;
+              border-radius: 0.5rem;
+              background: #FFFFFF;
+              color: hsl(240, 10%, 3.9%);
+          }
+          .button {
+              display: inline-block;
+              background-color: hsl(142.1, 76.2%, 36.3%);
+              color: hsl(355.7, 100%, 97.3%);
+              text-decoration: none;
+              padding: 10px 20px;
+              border-radius: 0.5rem;
+              font-weight: bolder;
+          }
+          .button:hover {
+              background-color: hsl(142.1, 76.2%, 36.3%);
+          }
+          .sub {
+            color: hsl(240, 5.9%, 10%);
+          }
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <h1>Verify Your Account</h1>
+          <p>Thank you for creating an account. Please click the button below to verify your email address.</p>
+          <p>
+              <a class="button" href="${link}">Verify Your Email</a>
+          </p>
+          <p class="sub">If you did not create an account, you can safely ignore this email.</p>
+      </div>
+  </body>
+  </html>
+  `
 }
